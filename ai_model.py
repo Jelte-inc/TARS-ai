83d29e5459658261d2942ef3afded204ab532a1c
Jelte <20029@calvijncollege.nl>
2025-09-12 16:06:31 +0200

    updated ai model

--- ai_model.py
+++ ai_model.py
@@ -1,5 +1,14 @@
 import ollama
 
-message = {'role': 'user', 'content': 'Why is the sky blue?'}
-for part in ollama.chat(model='gemma2:latest', messages=[message], stream=True):
-  print(part['message']['content'], end='', flush=True)
+message_content = ""
+
+while message_content != "bye bye": 
+  
+  message_content = input()
+ 
+  message = {'role': 'user', 'content': message_content}
+  try:
+    for part in ollama.chat(model='gemma3:4b', messages=[message], stream=True):
+      print(part['message']['content'], end='', flush=True)
+  except Exception as e:
+    print("something went wrong")
