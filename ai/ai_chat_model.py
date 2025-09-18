e06424cdb1f26c3e34c5e84789656f93f5b43c83
remco.zuidweg <remco.zuidweg@profects.com>
2025-09-18 20:14:11 +0200

    updated model parameter

--- ai/ai_chat_model.py
+++ ai/ai_chat_model.py
@@ -8,7 +8,7 @@ while message_content != "bye bye":
  
   message = {'role': 'user', 'content': message_content}
   try:
-    for part in ollama.chat(model='gemma3:4b', messages=[message], stream=True):
+    for part in ollama.chat(model='tars', messages=[message], stream=True):
       print(part['message']['content'], end='', flush=True)
   except Exception as e:
     print("something went wrong")
