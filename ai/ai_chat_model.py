ed2dbb27303cb126f704bcad18edd1033e1cdd79
Ezra Neeskens <20037@calvijncollege.nl>
2025-09-22 12:45:25 +0200

    added function call funcionality

--- ai/ai_chat_model.py
+++ ai/ai_chat_model.py
@@ -3,7 +3,7 @@ import ollama
 
 # Functie om modeloutput te verwerken
 def execute_model_output(model_output: str) -> str:
-    if model_output.strip() == "call_say_hi()":    
+    if model_output.strip() == "@":    
       return "hoi"
     else:
         return model_output
@@ -15,9 +15,18 @@ while message_content.lower() != "bye bye":
     message = {'role': 'user', 'content': message_content}
     try:
         full_response = ""
+        hoi = False
+        i = 0
         for part in ollama.chat(model='tars', messages=[message], stream=True):
+            if execute_model_output(full_response) == "hoi":
+                print("hoi")
+                hoi = True
+            elif len(full_response) > 1 and not hoi:
+              if i == 0:
+                print(full_response, end='')
+                i += 1
+              print(part['message']['content'], end='', flush=True)
             full_response += part['message']['content']
-            print(part['message']['content'], end='', flush=True)
         print('')  # Nieuwe regel na streaming
         # Verwerk de output voor functieaanroep
         result = execute_model_output(full_response)
