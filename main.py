f12a690afd90f7357f3f065a7fffd315496fb806
Jelte <20029@calvijncollege.nl>
2025-09-22 16:40:23 +0200

    updated listener and added to main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from ai.ai_chat_model import ai
 from speech.listener import speech_listener
 
 while True:
-    user_input = input()
+    user_input = speech_listener()
 
     walking_commands(user_input)
 
